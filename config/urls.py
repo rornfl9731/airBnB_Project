58edeec84fadea90a47285799d18494f633787c2
jinwoo <inx0930@gmail.com>
2019-11-28 04:18:59 +0900

    로그인, 로그아웃 완

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("rooms/",include('rooms.urls',namespace="rooms")),
+    path("users/", include("users.urls", namespace="users")),
     path("",include('core.urls',namespace="core"))
 ]
 
--- rooms/urls.py
+++ rooms/urls.py
@@ -8,5 +8,6 @@ app_name = "rooms"
 
 urlpatterns = [
     # path('<int:pk>', views.room_detail, name="detail")
-    path('<int:pk>',views.RoomDetail.as_view(),name="detail")
+    path('<int:pk>',views.RoomDetail.as_view(),name="detail"),
+    path('search/',views.SearchView.as_view(),name="search")
 ]
